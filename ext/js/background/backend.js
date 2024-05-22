e46094c02cbaf6010b11b2efb37a24ce3ee8a7a8
Stefan Vukovic <stefanvukovic44@gmail.com>
2024-05-22 21:26:14 +0200

    fix month number in anki filename

--- ext/js/background/backend.js
+++ ext/js/background/backend.js
@@ -2365,7 +2365,7 @@ export class Backend {
      */
     _ankNoteDateToString(date) {
         const year = date.getUTCFullYear();
-        const month = date.getUTCMonth().toString().padStart(2, '0');
+        const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
         const day = date.getUTCDate().toString().padStart(2, '0');
         const hours = date.getUTCHours().toString().padStart(2, '0');
         const minutes = date.getUTCMinutes().toString().padStart(2, '0');
